3178fea4f5d9178955e799a031b1201e611ea5e2
José Leandro <leandro.jlds@gmail.com>
2024-04-11 18:50:48 -0300

    Props and Hooks

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import Home from './paginas/home/Home';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
+function Home() {
+  const [completed, setCompleted] = useState(false);
+  const [tarefa, setTarefa] = useState('');
 
-function App() {
+  useEffect(() => {
+    if (completed) {
+      setTarefa('Parabéns! Você concluiu a tarefa!');
+    }
+  }, [completed]);
+
   return (
-   <Home />
+    <div>
+      <h1>Tarefa</h1>
+      <h3>{tarefa}</h3>
+      <p>Conclua a tarefa</p>
+      <button onClick={() => setCompleted(true)}>Concluir Tarefa</button>
+    </div>
   );
 }
 
-export default App;
+export default Home;
